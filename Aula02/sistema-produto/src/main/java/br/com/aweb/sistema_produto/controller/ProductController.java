38608b4568784b52ed053c7858bfdbf9a09c4239
LeandroGuidelli <leandrosoaresguidelli@gmail.com>
2025-08-20 22:41:16 -0300

    aula nova

--- Aula02/sistema-produto/src/main/java/br/com/aweb/sistema_produto/controller/ProductController.java
+++ Aula02/sistema-produto/src/main/java/br/com/aweb/sistema_produto/controller/ProductController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import br.com.aweb.sistema_produto.model.Product;
 import br.com.aweb.sistema_produto.service.ProductService;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
